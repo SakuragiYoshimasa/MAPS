431c4be43c0b376a14e3b71b1adc8621d64bd23c
Yoshimasa Sakuragi <ysrhsp@outlook.com>
2017-09-14 23:35:46 +0900

    wrote algorithm

--- Assets/CDT.cs
+++ Assets/CDT.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public static class CDT {
 	
 	public static List<Triangle> retriangulationFromRingByCDT(List<int> ring, List<Vector2> mapped_ring){
+
+		//領域を覆う大きな三角形を生成する
+		//制約線分に接するノードを一個ずつ選択し、以下の処理を行う
+		//　ノードDを内部に含む三角形ABCをABD,BCD,CADに分割する
+		//　ノードDに接するすべての三角形に対して変換処理２を行う、「変換される」、「変換されない」とも判断されなければ変換処理１を行う
+		//　ノードDに接するすべての三角形に対して要素辺と制約線分との幾何的な交差判定を行う
+		//　２辺が同一制約線分と交差している三角形が抽出された場合にはノードDの処理前の状態にメッシュを戻し、ノードD以外のノードを選択する
+		//　すべてのノードについて処理が終了した時点で領域の外に生成された三角形を除去し、メッシュを完成する
 		List<Triangle> added_triangles = new List<Triangle>();
 		int fow = 1;
 		int back = 1;
